2070ee6a2044b1538136e17e559c7c0caa5635bd
maamalama <bokarevstefan475@gmail.com>
2023-06-05 15:08:54 -0500

    added redis

--- pages/api/get-games.ts
+++ pages/api/get-games.ts
@@ -2,10 +2,23 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import axiosInstance from '../../utils/axios';
 import { Polybase } from '@polybase/client';
 import { v4 as uuidv4 } from 'uuid';
+import Redis from 'ioredis';
 
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<any>,) {
+
+    const redisUrl = process.env.NEXT_PUBLIC_REDIS as string;
+
+    const redis = new Redis(redisUrl);
+
+    const cached = await redis.get(`games`);
+
+    if (cached) {
+        res.status(200).json(JSON.parse(cached));
+        return;
+    }
+
     const db = new Polybase({
         defaultNamespace: "pk/0xa82e197a60581a388eab7a3c6668da651a404ea99073d9cbdfd7d596e2980bb457609498273a1cd3d9c37c613578279dc30192db9ec586c319b07084cd801cf2/direct3",
     });
@@ -21,6 +34,8 @@ export default async function handler(
 
 
     const games = await gamesColelction.get();
+
+    await redis.set(`games`, JSON.stringify(games), 'EX', 60 * 60 * 2);
 
     res.status(200).json(games);
 }
--- pages/api/get-nfts.ts
+++ pages/api/get-nfts.ts
@@ -1,12 +1,25 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { Polybase } from '@polybase/client';
 import { NetworkNames, Sdk, randomPrivateKey } from 'etherspot';
+import Redis from 'ioredis';
 
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<any>,) {
 
     const address = req.query.address as string;
+
+    const redisUrl = process.env.NEXT_PUBLIC_REDIS as string;
+
+    const redis = new Redis(redisUrl);
+
+    const cached = await redis.get(`nfts:${address}`);
+
+    if (cached) {
+        res.status(200).json(JSON.parse(cached));
+        return;
+    }
+
     const PRIVATE_KEY = randomPrivateKey();
     const sdk = new Sdk(PRIVATE_KEY, {
         networkName: NetworkNames.Mainnet
@@ -39,6 +52,8 @@ export default async function handler(
             }),
         };
     });
+
+    await redis.set(`nfts:${address}`, JSON.stringify(nfts), 'EX', 60 * 60 * 2);
 
     res.status(200).json(nfts);
 }
