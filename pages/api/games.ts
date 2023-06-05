9a13efe08df27eb16ec1495761ebc08ba1dfb452
maamalama <bokarevstefan475@gmail.com>
2023-06-04 22:52:53 -0500

    init

--- pages/api/games.ts
+++ pages/api/games.ts
@@ -3,4 +3,6 @@ import { NextApiRequest, NextApiResponse } from 'next';
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>,
-) {}
+) {
+  //TODO: get games from database
+}
