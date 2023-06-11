0c468461fdce14f16feb9f7e48cf1c821e2da9c7
zak <ledevzl@gmail.com>
2023-06-12 01:08:59 +0800

    feat: user joins a call

--- wrappers/HuddleWrapper.tsx
+++ wrappers/HuddleWrapper.tsx
@@ -1,9 +1,14 @@
 // this is not a wrapper for now but the full component
 
-import React, { useCallback, useEffect, useState } from "react";
-import { useEventListener, useHuddle01 } from "@huddle01/react";
+import React, { useRef, useEffect, useState } from "react";
 
-import { PhoneIcon, PlayIcon, LibraryIcon, SunIcon } from "@heroicons/react/outline";
+import { useEventListener, useHuddle01 } from "@huddle01/react";
+import { useDisplayName } from "@huddle01/react/app-utils";
+import { Audio, Video } from "@huddle01/react/components";
+
+import {
+  PhoneIcon
+} from "@heroicons/react/outline";
 import { IconButton } from "../component-library/components/IconButton/IconButton";
 import {
   useAudio,
@@ -16,9 +21,13 @@ import {
 } from "@huddle01/react/hooks";
 
 export const HuddleWrapper = () => {
-  const projectId = "KL1r3E1yHfcrRbXsT4mcE-3mK60Yc3YR";
+  // For dev, MUST use LOCALHOST instead of 0.0.0.0, for prod MUST use HTTPS
+
+  const projectId = "KL1r3E1yHfcrRbXsT4mcE-3mK60Yc3YR"; // test room id gis-jqho-jvs
   const { initialize } = useHuddle01();
   const { state, send } = useMeetingMachine();
+
+  const videoRef = useRef<HTMLVideoElement>(null);
 
   useEffect(() => {
     initialize(projectId);
@@ -31,12 +40,30 @@ export const HuddleWrapper = () => {
 
   const [shouldJoinRoom, setShouldJoinRoom] = useState(false); // could be better
 
+  const {
+    fetchAudioStream,
+    produceAudio,
+    stopAudioStream,
+    stopProducingAudio,
+    stream: micStream,
+  } = useAudio();
+  const {
+    fetchVideoStream,
+    produceVideo,
+    stopVideoStream,
+    stopProducingVideo,
+    stream: camStream,
+  } = useVideo();
   const { joinRoom, leaveRoom } = useRoom();
 
+  const [displayNameText, setDisplayNameText] = useState("Participant");
+  const { setDisplayName, error: displayNameError } = useDisplayName();
+  const { peers } = usePeers();
+
   // Event Listner
-  // useEventListener("lobby:cam-on", () => {
-  //   if (camStream && videoRef.current) videoRef.current.srcObject = camStream;
-  // });
+  useEventListener("lobby:cam-on", () => {
+    if (camStream && videoRef.current) videoRef.current.srcObject = camStream;
+  });
 
   useEventListener("room:joined", () => {
     console.log("room:joined");
@@ -66,15 +93,43 @@ export const HuddleWrapper = () => {
 
     console.log("roomId: ", roomId);
     setRoomId(roomId);
-  }
+  };
 
   useEffect(() => {
-    if(joinLobby.isCallable && roomId.length > 2) joinLobby(roomId);
+    if (joinLobby.isCallable && roomId.length > 2) joinLobby(roomId);
   }, [joinLobby.isCallable, roomId]);
 
   useEffect(() => {
-    if(joinRoom.isCallable) joinRoom();
-  }, [joinRoom.isCallable]);
+    if (fetchAudioStream.isCallable) fetchAudioStream(); // this bugs if not called before joining a room and fetching video
+  }, [fetchAudioStream.isCallable]);
+
+  const [fetchedAudio, setFetchedAudio] = useState(false);
+  
+  useEffect(() => {
+    if (!fetchedAudio && micStream) setFetchedAudio(true); 
+  }, [micStream, fetchedAudio]);
+
+  useEffect(() => {
+    if (produceAudio.isCallable) produceAudio(micStream);
+  }, [produceAudio.isCallable]);
+
+  useEffect(() => {
+    if (joinRoom.isCallable && micStream && camStream) joinRoom(); // need mic and video to join room otherwise it bugs
+  }, [joinRoom.isCallable, micStream, camStream]);
+
+  useEffect(() => {
+    if (fetchVideoStream.isCallable && fetchedAudio) fetchVideoStream(); // this bugs if not called before joining a room
+  }, [fetchVideoStream.isCallable, fetchedAudio]);
+
+  useEffect(() => {
+    if (produceVideo.isCallable) produceVideo(camStream);
+  }, [produceVideo.isCallable]);
+
+
+  useEffect(() => {
+    // Probably not necessary but kept in case
+    if (setDisplayName.isCallable) setDisplayName(displayNameText);
+  }, [setDisplayName.isCallable]);
 
   return (
     <div className="flex items-center px-2 md:px-4 py-3 border-l-0 z-10 max-md:h-fit md:max-h-sm  h-16">
@@ -86,6 +141,31 @@ export const HuddleWrapper = () => {
         testId="new-message-icon-cta"
         srText={"Call"}
       />
+
+      <div>
+        Me Video:
+        <video ref={videoRef} autoPlay muted></video>
+        <div className="grid grid-cols-4">
+          {Object.values(peers)
+            .filter((peer) => peer.cam)
+            .map((peer) => (
+              <>
+                role: {peer.role}
+                <Video
+                  key={peer.peerId}
+                  peerId={peer.peerId}
+                  track={peer.cam}
+                  debug
+                />
+              </>
+            ))}
+          {Object.values(peers)
+            .filter((peer) => peer.mic)
+            .map((peer) => (
+              <Audio key={peer.peerId} peerId={peer.peerId} track={peer.mic} />
+            ))}
+        </div>
+      </div>
     </div>
   );
 };
